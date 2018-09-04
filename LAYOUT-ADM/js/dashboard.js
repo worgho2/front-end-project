009e8028a0cb931432661b1402b5f24a572f6648
Otavio Baziewicz Filho <tavinho_baza@hotmail.com>
2018-09-04 16:50:15 -0300

    last

---
+++ LAYOUT-ADM/js/dashboard.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+	//Apresenta ou ocultar o menu
+    $('.sidebar-toggle').on('click', function () {
+        $('.sidebar').toggleClass('toggled');
+    });
+
+    //carregar aberto o submenu
+    var active = $('.sidebar .active');
+    if (active.length && active.parent('.collapse').length) {
+    	var parent = active.parent('.collapse');
+    	parent.prev('a').attr('aria-expanded', true);
+    	parent.addClass('show');
+    }
+});
