20d204fd92881f779ecc3daecff475d747921997
Otavio Baziewicz Filho <tavinho_baza@hotmail.com>
2018-09-04 16:48:08 -0300

    first

---
+++ LAYOUT/index.php
@@ -0,0 +1,12 @@
+<?php
+	echo "<br> <br> exercicio 01 <br>";
+	$nome = "sabonete";
+	$sobrenome = "liquido";
+	echo "Nome: " . $nome . "<br> Sobrenome: " . $sobrenome;
+
+	echo "<br> <br> exercicio 02 <br>";
+	$nome = "plastico";
+	$salario = 1.99;
+	$cargo = "Bar-man";
+	echo "Nome: " . $nome . "<br> Salario: " . $salario . "<br> Cargo: " . $cargo;
+?>
